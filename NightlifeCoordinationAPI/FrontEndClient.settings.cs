a04a0f68f86ac63b190f2bbf792193c86c615294
nivwer <nivwerr@gmail.com>
2024-03-02 00:18:58 -0300

    Configured secret keys.

---
+++ NightlifeCoordinationAPI/FrontEndClient.settings.cs
@@ -0,0 +1,6 @@
+namespace NightlifeCoordinationAPI.FrontEndClient.settings;
+
+public class FrontEndClientSettings
+{
+    public required string BaseUrl { get; set; }
+}
---
+++ NightlifeCoordinationAPI/YelpAPI.settings.cs
@@ -0,0 +1,7 @@
+namespace NightlifeCoordinationAPI.YelpAPI.settings;
+
+public class YelpAPISettings
+{
+    public required string Key { get; set; }
+    public required string BaseUrl { get; set; }
+}
