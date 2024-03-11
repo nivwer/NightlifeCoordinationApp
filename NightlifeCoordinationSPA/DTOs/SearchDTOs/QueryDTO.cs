8ec8a200c6dbdb55152d50f83342af4dd29c6742
nivwer <nivwerr@gmail.com>
2024-03-10 21:01:10 -0300

    updated Search layout.

---
+++ NightlifeCoordinationSPA/DTOs/SearchDTOs/QueryDTO.cs
@@ -0,0 +1,7 @@
+namespace NightlifeCoordinationSPA.DTOs.SearchDTOs;
+
+public class QueryDTO
+{
+    public string? Keyword { get; set; }
+    public string? Location { get; set; }
+}
--- NightlifeCoordinationSPA/Layout/Layouts/SearchLayout/SearchLayout.razor.cs
+++ NightlifeCoordinationSPA/Layout/Layouts/SearchLayout/SearchLayout.razor.cs
@@ -1,6 +1,28 @@
+using Microsoft.AspNetCore.Components;
+using NightlifeCoordinationSPA.DTOs.SearchDTOs;
+
 namespace NightlifeCoordinationSPA.Layout.Layouts.SearchLayout;
 
-public partial class SearchLayout 
+public partial class SearchLayout
 {
+    public QueryDTO Model = new QueryDTO();
+
     public bool DrawerOpen = true;
+
+    [Inject]
+    public NavigationManager? Navigate { get; set; }
+
+    // protected override async Task OnInitializedAsync()
+    // {
+    // CARGAR LOS PARAMS
+    // }
+
+    public void OnValidSubmit()
+    {
+        if (Model != null && !string.IsNullOrWhiteSpace(Model.Keyword))
+        {
+            StateHasChanged();
+            Navigate!.NavigateTo($"/results?query={Model.Keyword}&location={Model.Location}");
+        }
+    }
 }
